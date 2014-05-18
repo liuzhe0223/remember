b480e14d592e6645f3d095c4ab38d470722fb6b5
liuzhe <liuzhe0223@gmail.com>
2014-05-18 16:30:10 +0000

    add utils_test

--- handler/handler.go
+++ handler/handler.go
@@ -1,26 +1,26 @@
 package handler
 
-import (
-	"github.com/liuzhe0223/remember/dt"
-	"io"
-	"net/http"
-)
+// import (
+// 	"github.com/liuzhe0223/remember/dt"
+// 	"io"
+// 	"net/http"
+// )
 
-func Handler(w http.ResponseWriter, r *http.Request) {
+// func Handler(w http.ResponseWriter, r *http.Request) {
 
-	method, key, op, data, params, err := ParseRquest(r)
+// 	method, key, op, data, params, err := ParseRquest(r)
 
-	value, ok := Db[key]
+// 	value, ok := Db[key]
 
-	//this key is not in the db
-	if !ok && method == "Get" {
-		io.WriteString(w, "")
-		return
-	}
+// 	//this key is not in the db
+// 	if !ok && method == "Get" {
+// 		io.WriteString(w, "")
+// 		return
+// 	}
 
-	resValues, err := DoOp(&value, method, op, params)
+// 	resValues, err := DoOp(&value, method, op, params)
 
-	resString := ParseRobj(&resValue)
+// 	resString := ParseRobj(&resValue)
 
-	io.WriteString(w, resString)
-}
+// 	io.WriteString(w, resString)
+// }
--- handler/utils.go
+++ handler/utils.go
@@ -2,28 +2,29 @@ package handler
 
 import (
 	"encoding/json"
+	"errors"
 	"github.com/liuzhe0223/remember/dt"
+	"io/ioutil"
 	"net/http"
-	"net/url"
 	"reflect"
 	"strings"
 )
 
 type RequestData struct {
-	Data interface{}
+	Data interface{} `json:"data"`
 }
 
-func ParseRequest(r *http.Request) (method, key, op string, data RequestData, params map[string]string, err error) {
+func ParseRequest(r *http.Request) (method, key, op string, data interface{}, params map[string]string, err error) {
 	//for the root path (or index)
 	method = r.Method
-	if r.Url.Path == "/" {
+	if r.URL.Path == "/" {
 		op = "default"
 		return
 	}
 
 	//op and key
-	path := string.Trim(r.URL.Path.T, "/")
-	spList := string.Split(path)
+	path := strings.Trim(r.URL.Path, "/")
+	spList := strings.Split(path, "/")
 	switch len(spList) {
 	case 1:
 		key = spList[0]
@@ -32,13 +33,14 @@ func ParseRequest(r *http.Request) (method, key, op string, data RequestData, pa
 		key = spList[0]
 		op = spList[1]
 	default:
-		err = Error("wrong url")
+		err = errors.New("wrong url")
 		return
 	}
 
 	//data
 	requestData := new(RequestData)
-	err = json.Unmarshal(r.Body.Read(), requestData)
+	body, _ := ioutil.ReadAll(r.Body)
+	err = json.Unmarshal(body, requestData)
 	if err != nil {
 		data = requestData.Data
 	}
@@ -46,7 +48,7 @@ func ParseRequest(r *http.Request) (method, key, op string, data RequestData, pa
 	//parameters
 	params = map[string]string{}
 	for k, v := range r.Form {
-		params[k] = v
+		params[k] = v[0]
 	}
 
 	return
@@ -61,29 +63,49 @@ var methodMap = map[string]map[string]string{
 }
 
 func getRealOpAndParams(opAndParamsStr string, inParams map[string]string) (realOp string, outParams []reflect.Value) {
-  splitList := strings.Split(opAndParams, ":")
-  outParams = make([]reflect.Value, 0, 2)
-  realOp = splitList[0]
-  for _, el := range inParams[1:] {
-    outParams = append(outParams, reflect.ValueOf(inParams[el]))
-  }
-  return
-}
-
-func DoOp(robj *Robj, method, op string, params map[string][string]) (resValue []Robj) {
-
-  realOp, in := getRealOpAndParams(methodMap[op], params)
-
-	//todo: ad more op
-	switch robj.Type {
-	case dt.RlistType:
-		resValue = reflect.Value(robj.Obj.(dt.Rlist)).MethodByName(trueOp).CallSlice(in)
-	default:
-		resValue = ""
+	splitList := strings.Split(opAndParamsStr, ":")
+	outParams = make([]reflect.Value, 0, 2)
+	realOp = splitList[0]
+	for _, el := range splitList[1:] {
+		outParams = append(outParams, reflect.ValueOf(inParams[el]))
 	}
 	return
 }
 
-func ParseRobj(robj *Rboj) (jsonStr string) {
+func DoOp(robj *dt.Robj, method, op string, params map[string]string) (resValue []dt.Robj) {
 
+	realOp, in := getRealOpAndParams(methodMap[method][op], params)
+
+	var resReflectValues []reflect.Value
+	//todo: ad more op
+	switch robj.Type {
+	case dt.RlistType:
+		resReflectValues = reflect.ValueOf(robj.Obj.(dt.Rlist)).MethodByName(realOp).CallSlice(in)
+	default:
+		resReflectValues = make([]reflect.Value, 0, 0)
+	}
+
+	//convent the res to []dt.Robj form []relect.Value
+	resValue = make([]dt.Robj, 0, len(resReflectValues))
+	for _, value := range resReflectValues {
+		valueInterface := value.Interface()
+		typedValue, _ := valueInterface.(dt.Robj)
+		resValue = append(resValue, typedValue)
+	}
+	return
+}
+
+//parse base robj, int and string
+func ParseBaseRobj(robj *dt.Robj) (jsonBytes []byte) {
+	switch robj.Type {
+	case dt.RintType:
+		intValue, _ := robj.Obj.(int)
+		data2Int := map[string]int{"data": intValue}
+		jsonBytes, _ = json.Marshal(data2Int)
+	case dt.RstringType:
+		stringValue, _ := robj.Obj.(string)
+		data2String := map[string]string{"data": stringValue}
+		jsonBytes, _ = json.Marshal(data2String)
+	}
+	return
 }
