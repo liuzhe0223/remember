5b1afcee96539e0085427a1ca9d942227f7fe9ed
liuzhe <liuzhe0223@gmail.com>
2014-05-21 12:52:59 +0000

    do op test pass

--- handler/utils.go
+++ handler/utils.go
@@ -62,7 +62,7 @@ var methodMap = map[string]map[string]string{
 	},
 }
 
-func getRealOpAndParams(opAndParamsStr string, inParams map[string]string) (realOp string, outParams []reflect.Value) {
+func getRealOpAndParams(opAndParamsStr string, inParams map[string]interface{}) (realOp string, outParams []reflect.Value) {
 	splitList := strings.Split(opAndParamsStr, ":")
 	outParams = make([]reflect.Value, 0, 2)
 	realOp = splitList[0]
@@ -72,26 +72,25 @@ func getRealOpAndParams(opAndParamsStr string, inParams map[string]string) (real
 	return
 }
 
-func DoOp(robj *dt.Robj, method, op string, params map[string]string) (resValue []dt.Robj) {
+func DoOp(robj *dt.Robj, method, op string, params map[string]interface{}) (resReflectValues []reflect.Value) {
 
 	realOp, in := getRealOpAndParams(methodMap[method][op], params)
 
-	var resReflectValues []reflect.Value
 	//todo: ad more op
 	switch robj.Type {
 	case dt.RlistType:
-		resReflectValues = reflect.ValueOf(robj.Obj.(dt.Rlist)).MethodByName(realOp).CallSlice(in)
+		resReflectValues = reflect.ValueOf(robj.Obj.(*dt.Rlist)).MethodByName(realOp).Call(in)
 	default:
 		resReflectValues = make([]reflect.Value, 0, 0)
 	}
 
 	//convent the res to []dt.Robj form []relect.Value
-	resValue = make([]dt.Robj, 0, len(resReflectValues))
-	for _, value := range resReflectValues {
-		valueInterface := value.Interface()
-		typedValue, _ := valueInterface.(dt.Robj)
-		resValue = append(resValue, typedValue)
-	}
+	// resValue = make([]dt.Robj, 0, len(resReflectValues))
+	// for _, value := range resReflectValues {
+	// 	valueInterface := value.Interface()
+	// 	typedValue, _ := valueInterface.(dt.Robj)
+	// 	resValue = append(resValue, typedValue)
+	// }
 	return
 }
 
--- handler/utils_test.go
+++ handler/utils_test.go
@@ -8,20 +8,19 @@ import (
 
 func TestGetRealOPAndParams(t *testing.T) {
 	opAndParamsStr := "Lrange:start:end"
-	inParams := map[string]string{
-		"start": "0",
-		"end":   "6",
+	inParams := map[string]interface{}{
+		"start": 0,
+		"end":   6,
 	}
 
 	realOp, outParmas := getRealOpAndParams(opAndParamsStr, inParams)
 	assert.Equal(t, "Lrange", realOp)
 
-	testOutParmas := []string{"0", "6"}
-	outStrParmas := make([]string, 0, 2)
+	testOutParmas := []interface{}{0, 6}
+	outStrParmas := make([]interface{}, 0, 2)
 	for _, value := range outParmas {
 		valueInterface := value.Interface()
-		valueStr, _ := valueInterface.(string)
-		outStrParmas = append(outStrParmas, valueStr)
+		outStrParmas = append(outStrParmas, valueInterface)
 	}
 
 	assert.Equal(t, testOutParmas, outStrParmas)
@@ -33,4 +32,26 @@ func TestParseBaseRobj(t *testing.T) {
 
 	assert.Equal(t, string(ParseBaseRobj(rintObj)), "{\"data\":3}")
 	assert.Equal(t, string(ParseBaseRobj(rstringObj)), "{\"data\":\"test\"}")
+}
+
+func TestDoOP(t *testing.T) {
+	rlist := dt.NewRlist()
+
+	robj1 := dt.Robj{dt.RintType, 1}
+	robj2 := dt.Robj{dt.RintType, 2}
+	robj3 := dt.Robj{dt.RintType, 3}
+
+	rlist.Rpush(robj1)
+	rlist.Rpush(robj2)
+	rlist.Rpush(robj3)
+
+	params := map[string]interface{}{
+		"start": 1,
+		"end":   2,
+	}
+
+	rlistObj := dt.Robj{dt.RlistType, rlist}
+	opRes := DoOp(&rlistObj, "GET", "lrange", params)
+	res, _ := opRes[0].Interface().([]dt.Robj)
+	assert.Equal(t, []dt.Robj{robj2, robj3}, res)
 }
